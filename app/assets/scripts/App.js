11e5ba6436e2bf79847ff7b676f4f79634b5f37e
jd3 <contact@joedomingo.online>
2017-01-23 07:50:03 -0500

    Just cleaning up, before starting Lecture 41, How to Avoid JQuery Spaghetti

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,20 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+// var Person = require('./modules/Person'); // this is via native NodeJS
+import Person from './modules/Person'; // this is the new ES6 way of "requiring" modules
+
+class Adult extends Person {
+	payTaxes() {
+		console.log(this.name + " no owes $0 in taxes.");
+	}
+}
 
 // alert("We are the world. We are the children.");
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "orange");
 jane.greet();
-
+jane.payTaxes();
 
 $("h1").remove();
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,13 +1,32 @@
 // constructor
-function Person(fullName, favColor) {
-	this.name = fullName;
-	this.favoriteColor = favColor;
-	this.greet = function() {
-		console.log("Hello there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+//  THIS IS JavaScript 5
+// function Person(fullName, favColor) {
+// 	this.name = fullName;
+// 	this.favoriteColor = favColor;
+// 	this.greet = function() {
+// 		console.log("Hello0000 there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+// 	}
+// } 
+
+/* BELOW IS THE ES6 VERSION */
+class Person { // note: no () here since this is a class
+	
+	// class constructor
+	constructor(fullName, favColor) {
+		this.name = fullName;
+		this.favoriteColor = favColor;
+	}
+	
+	// accessors/mutators
+	// methods here 
+	greet() {
+		console.log("Huy!, ako ay si " + this.name + " at ako ay isang kulay na " + this.favoriteColor + ".");
 	}
 }
 
-module.exports = Person;
+
+// module.exports = Person; // the old node.js way
+export default Person; // the new ES6 way
 
 
 
