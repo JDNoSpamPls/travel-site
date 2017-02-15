f095a02df619b9db30b53ea680a497c047119b9f
jd3 <contact@joedomingo.online>
2017-02-15 13:31:04 -0500

    Completed sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,11 @@ the name MobileMenu is technically a variable
 and can be changed to anything you want, but 
 it's best to stay semantic so MobileMenu it is. */
 import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll.js'; 
+import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "84%");
 new RevealOnScroll($(".testimonial"), "72%");
+var stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,85 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
+
+class StickyHeader {
+
+	constructor() {
+		this.siteHeader = $(".site-header");
+		this.headerTriggerElement = $(".large-hero__title");
+		this.createHeaderWaypoint();
+		this.pageSections = $(".page-section");
+		this.headerLinks = $(".primary-nav a");
+		this.homeTriggerElement = $("#home");
+		this.createSectionWaypoints();
+		this.addSmoothScrolling();
+		this.createHomeWaypoint();
+	}
+
+	addSmoothScrolling() {
+		this.headerLinks.smoothScroll();
+	}
+
+	// reset "home" link
+	createHomeWaypoint() {
+		var that = this;
+		var w = $(window);
+		new Waypoint({
+			element: this.homeTriggerElement[0],
+			handler: function(direction) {
+				w.on("scroll", function() {
+					if((w.scrollTop()) < 360) {
+						that.headerLinks.removeClass("is-current-link");
+					}
+				});
+			},
+			// offset: "50%"
+		}); 
+	}
+
+	createHeaderWaypoint() {
+		var that = this;
+		new Waypoint({
+			element: this.headerTriggerElement[0],
+			handler: function(direction) {
+				if (direction == "down") {
+					that.siteHeader.addClass("site-header--dark");
+				} else {
+					that.siteHeader.removeClass("site-header--dark");
+				}
+			}
+		}); 
+	}
+
+	createSectionWaypoints() {
+		var that = this;
+		this.pageSections.each(function() {
+			var currentPageSection = this;
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if (direction === "down") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					}
+				},
+				offset: "18%" 
+			});
+
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if (direction === "up") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					}
+				},
+				offset: "-80%" 
+			});
+		});
+	}
+}
+
+export default StickyHeader;
