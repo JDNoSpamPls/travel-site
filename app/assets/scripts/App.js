88652040df55313d20ab2c6d4e1eb89f037a3fd4
jd3 <contact@joedomingo.online>
2017-02-17 10:02:44 -0500

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,9 +5,11 @@ it's best to stay semantic so MobileMenu it is. */
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js';
+import Modal from './modules/Modal.js';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "84%");
 new RevealOnScroll($(".testimonial"), "72%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
