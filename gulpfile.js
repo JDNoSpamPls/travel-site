faa78d9b94682e59dcac5a686a176f0bd8ed4289
jd3 <contact@joedomingo.online>
2017-02-17 16:20:56 -0500

    Finished build task producing 'dist' folder.

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,6 @@ require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
 require('./gulp/tasks/modernizr');
+require('./gulp/tasks/build');
 
 // define default tasks below
